e4f2327609985eee75f59e579ebdfe12abc6a7e1
flybywiremod-boop <flybywiremod@gmail.com>
2026-01-19 19:11:30 +0100

    created project

---
+++ src/main/java/net/flybywire/createkineticlift/CreateKineticLift.java
@@ -0,0 +1,63 @@
+package net.flybywire.createkineticlift;
+
+import com.mojang.logging.LogUtils;
+import net.flybywire.createkineticlift.block.ModBlocks;
+import net.flybywire.createkineticlift.item.ModCreativeModTabs;
+import net.flybywire.createkineticlift.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(CreateKineticLift.MOD_ID)
+public class CreateKineticLift {
+    public static final String MOD_ID = "createkineticlift";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreateKineticLift() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
---
+++ src/main/java/net/flybywire/createkineticlift/item/ModCreativeModTabs.java
@@ -0,0 +1,41 @@
+package net.flybywire.createkineticlift.item;
+
+import net.flybywire.createkineticlift.CreateKineticLift;
+import net.flybywire.createkineticlift.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.awt.*;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreateKineticLift.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> CREATE_TAB = CREATIVE_MODE_TABS.register("create_tab" ,
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.CONTROL_CHAIR.get()))
+            .title(Component.translatable("Create: Kinetic Lift"))
+            .displayItems((pParameters, pOutput) -> {
+                pOutput.accept(ModItems.SAPPHIRE.get());
+                pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+                pOutput.accept(Items.DIAMOND);
+
+                pOutput.accept(ModBlocks.CONTROL_CHAIR.get());
+                pOutput.accept(ModBlocks.INVERTED_CONTROL_CHAIR.get());
+
+
+            })
+            .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
