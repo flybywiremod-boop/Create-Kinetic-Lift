1eb069382a8afa825fb7c146611ab761b942dcba
atroniax <140010479+atroniax@users.noreply.github.com>
2026-02-14 00:25:28 +0100

    Fix Red Cable

--- src/main/java/net/flybywire/createkineticlift/computercable/RedCable.java
+++ src/main/java/net/flybywire/createkineticlift/computercable/RedCable.java
@@ -3,4 +3,7 @@ package net.flybywire.createkineticlift.computercable;
 import net.minecraft.world.item.Item;
 
 public class RedCable extends Item {
+    public RedCable(Properties properties) {
+        super(properties);
+    }
 }
