b999139d18d3bdff82741dc0117e7ee0ca8bf48f
flybywiremod-boop <flybywiremod@gmail.com>
2026-08-03 19:59:40 +0200

    tried registering turbofan engine as a test but couldnt figure out

---
+++ src/main/java/net/flybywire/createkineticlift/content/turbofan_engine/TurbofanEngineItem.java
@@ -0,0 +1,9 @@
+package net.flybywire.createkineticlift.content.turbofan_engine;
+
+import net.minecraft.world.item.Item;
+
+public class TurbofanEngineItem extends Item {
+
+	public TurbofanEngineItem (Properties properties) { super(properties);
+	}
+}
